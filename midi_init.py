3b3a4ca5f00d31d6e8a53a775b09fe7e2c47371a
Scott Ammon <scott.o.ammon@gmail.com>
2020-04-07 21:05:47 -0700

    starting to refactor with classes

--- midi_init.py
+++ midi_init.py
@@ -1,37 +1,10 @@
 import midi
-from transforms import transform_event
+
+from process_events import MidiTrack
 
 pattern = midi.read_midifile("basic_drum.mid")
 
-# Instantiate new track to write MIDI to
-new_pattern = midi.Pattern(tracks=[], resolution=pattern.resolution, format=1, tick_relative=True)
-track = midi.Track()
-new_pattern.append(track)
+midi_track = MidiTrack(pattern)
+midi_track.process_midi_events()
 
-# TODO Extract this to a separate function eventIterator(pattern)
-diff_array = []
-# Modify each note event in the single track located at pattern[0]
-for event in pattern[0]:
-  if type(event) is midi.events.NoteOnEvent:
-    new_note_on = transform_event(event)
-    track.append(new_note_on)
-    diff_array.append({ 
-      "inst": event.data[0], 
-      "diff": new_note_on.tick - event.tick
-    })
-  elif type(event) is midi.events.NoteOffEvent:
-    for obj in diff_array:
-      if obj["inst"] == event.data[0]:
-        new_tick = event.tick + obj["diff"]
-        diff_array.remove(obj)
-      else:
-        new_tick = event.tick
-    new_note_off = midi.NoteOffEvent(tick=new_tick, velocity=event.data[1], pitch=event.data[0])
-    track.append(new_note_off)
-  else:
-    track.append(event)
-
-# Add end of track event and write out the transformed MIDI file
-end_of_track = midi.EndOfTrackEvent(tick=1)
-track.append(end_of_track)
-midi.write_midifile("output_transformed.mid", new_pattern)
+midi.write_midifile("output_transformed.mid", midi_track.new_pattern)
